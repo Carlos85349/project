174bb03c14ae17b95aad418fc99faa6dd4908575
carcamocarlosalberto974 <carcamocarlosalberto974@gmail.com>
2024-12-04 00:41:26 +0000

    Adicion de funcionalidad de drawer menu

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -5,16 +5,77 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
     return Scaffold(
+        key: scaffoldKey,
         appBar: AppBar(
           centerTitle: true,
           backgroundColor: Colors.green,
           title: const Text("Bienvenido, usuario"),
-          leading: const Icon(Icons.menu),
-          actions: const [
-            Icon(Icons.search),
-            Icon(Icons.more_vert),
+          leading: IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                scaffoldKey.currentState?.openDrawer();
+              }),
+          actions: [
+            IconButton(icon: const Icon(Icons.notifications), onPressed: () {}),
+            IconButton(icon: const Icon(Icons.more_vert), onPressed: () {}),
           ],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const [
+              DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                  ),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 40,
+                        backgroundColor: Colors.lightGreen,
+                        child: Icon(
+                          Icons.pets,
+                          color: Colors.white,
+                          size: 50,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Menu",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                        ),
+                      ),
+                    ],
+                  )),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text("Inicio"),
+              ),
+              ListTile(
+                leading: Icon(Icons.search),
+                title: Text("Buscar"),
+              ),
+              ListTile(
+                leading: Icon(Icons.calendar_month),
+                title: Text("Calendario"),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text("Configuracion"),
+              ),
+              ListTile(
+                leading: Icon(Icons.logout),
+                title: Text("Cerrar Sesion"),
+              ),
+            ],
+          ),
         ),
         body: PageView(
           children: const [
