943886848b32d1d661ad77da25dba3d2ec60db80
Carlos85349 <147650880+Carlos85349@users.noreply.github.com>
2024-12-08 08:31:05 -0600

    Autenticación (pendiente de terminar)

--- lib/src/pages/register_page.dart
+++ lib/src/pages/register_page.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/src/widgets/custom_input.dart';
+import 'package:provider/provider.dart';
+import 'package:myapp/src/providers/login_provider.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Controladores para los campos de registro
+    final TextEditingController _nameController = TextEditingController();
+    final TextEditingController _phoneController = TextEditingController();
+    final TextEditingController _emailController = TextEditingController();
+    final TextEditingController _passwordController = TextEditingController();
+    final TextEditingController _confirmPasswordController = TextEditingController();
+    final TextEditingController _petNameController = TextEditingController();
+    final TextEditingController _petSpeciesController = TextEditingController();
+    final TextEditingController _petBreedController = TextEditingController();
+    final TextEditingController _petAgeController = TextEditingController();
+    final TextEditingController _petGenderController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
@@ -94,77 +108,83 @@ class RegisterPage extends StatelessWidget {
                           height: 25,
                         ),
                         Container(
-                            padding: const EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: const [
-                                  BoxShadow(
-                                    color: Color.fromRGBO(12, 164, 157, 0.552),
-                                    blurRadius: 20,
-                                    offset: Offset(0, 10),
-                                  )
-                                ]),
-                            child: const Column(
-                              children: <Widget>[
-                                SizedBox(
-                                  height: 25,
-                                  child: Text("Datos personales:"),
-                                ),
-                                CustomInput(
-                                  title: 'Nombre Completo ',
-                                  icon: Icons.person,
-                                ),
-                                CustomInput(
-                                  title: 'Numero de telefono',
-                                  icon: Icons.phone,
-                                ),
-                                CustomInput(
-                                  title: 'Correo electronico',
-                                  icon: Icons.email,
-                                ),
-                                CustomInput(
-                                  title: 'Contraseña',
-                                  icon: Icons.password,
-                                ),
-                                CustomInput(
-                                  title: 'Confirmar Contraseña',
-                                  icon: Icons.password,
-                                ),
-                                SizedBox(
-                                  height: 20,
-                                ),
-                                SizedBox(
-                                  height: 25,
-                                  child: Text("Datos de tu mascota:"),
-                                ),
-                                CustomInput(
-                                  title: 'Nombre',
-                                  icon: Icons.person,
-                                ),
-                                CustomInput(
-                                  title: 'Especie',
-                                  icon: Icons.pets,
-                                ),
-                                CustomInput(
-                                  title: 'Raza',
-                                  icon: Icons.category,
-                                ),
-                                CustomInput(
-                                  title: 'Edad',
-                                  icon: Icons.calendar_today,
-                                ),
-                                CustomInput(
-                                  title: 'Sexo',
-                                  icon: Icons.transgender,
-                                ),
-                              ],
-                            )),
-                        const SizedBox(
-                          height: 40,
+                          padding: const EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Color.fromRGBO(12, 164, 157, 0.552),
+                                  blurRadius: 20,
+                                  offset: Offset(0, 10),
+                                )
+                              ]),
+                          child: Column(
+                            children: <Widget>[
+                              CustomInput(
+                                title: 'Nombre Completo ',
+                                icon: Icons.person,
+                                controller: _nameController,
+                              ),
+                              CustomInput(
+                                title: 'Número de teléfono',
+                                icon: Icons.phone,
+                                controller: _phoneController,
+                              ),
+                              CustomInput(
+                                title: 'Correo electrónico',
+                                icon: Icons.email,
+                                controller: _emailController,
+                              ),
+                              CustomInput(
+                                title: 'Contraseña',
+                                icon: Icons.password,
+                                obscureText: true,
+                                controller: _passwordController,
+                              ),
+                              CustomInput(
+                                title: 'Confirmar Contraseña',
+                                icon: Icons.password,
+                                obscureText: true,
+                                controller: _confirmPasswordController,
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              const SizedBox(
+                                height: 25,
+                                child: Text("Datos de tu mascota:"),
+                              ),
+                              CustomInput(
+                                title: 'Nombre de mascota',
+                                icon: Icons.pets,
+                                controller: _petNameController,
+                              ),
+                              CustomInput(
+                                title: 'Especie',
+                                icon: Icons.pets,
+                                controller: _petSpeciesController,
+                              ),
+                              CustomInput(
+                                title: 'Raza',
+                                icon: Icons.category,
+                                controller: _petBreedController,
+                              ),
+                              CustomInput(
+                                title: 'Edad',
+                                icon: Icons.calendar_today,
+                                controller: _petAgeController,
+                              ),
+                              CustomInput(
+                                title: 'Sexo',
+                                icon: Icons.transgender,
+                                controller: _petGenderController,
+                              ),
+                            ],
+                          ),
                         ),
                         const SizedBox(
-                          height: 20,
+                          height: 40,
                         ),
                         ElevatedButton(
                           style: ElevatedButton.styleFrom(
@@ -173,7 +193,30 @@ class RegisterPage extends StatelessWidget {
                               borderRadius: BorderRadius.circular(20),
                             ),
                           ),
-                          onPressed: () {},
+                          onPressed: () {
+                            // Llamar al provider para registrar el usuario
+                            final loginProvider = Provider.of<LoginProvider>(context, listen: false);
+                            loginProvider.registerUser(
+                              name: _nameController.text,
+                              phone: _phoneController.text,
+                              email: _emailController.text,
+                              password: _passwordController.text,
+                              confirmPassword: _confirmPasswordController.text,
+                              petName: _petNameController.text,
+                              petSpecies: _petSpeciesController.text,
+                              petBreed: _petBreedController.text,
+                              petAge: _petAgeController.text,
+                              petGender: _petGenderController.text,
+                              onSuccess: () {
+                                // Si el registro fue exitoso
+                                Navigator.pushNamed(context, '/home');
+                              },
+                              onError: (error) {
+                                // Mostrar error si algo sale mal
+                                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(error)));
+                              },
+                            );
+                          },
                           child: Container(
                             height: 50,
                             margin: const EdgeInsets.symmetric(horizontal: 50),
